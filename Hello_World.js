e8f3b0607f31b9aa3bd01d62bdfcda77d691351a
Varsha Tantri <varsha_tantri@mckinsey.com>
2018-01-16 10:17:28 +0530

    Upper Case array

--- Hello_World.js
+++ Hello_World.js
@@ -4,12 +4,16 @@ console.log("Return Upper Case (Failure)",Upper(12)===null)
 console.log("Return Upper Case (Pass)",Upper('hi there')===true)
 //test case 3
 console.log("Return Upper Case (Blank string)",Upper('')===true)
+//test case 4
+console.log("Return Upper Case (array)",Upper([1,2,3,4])===null)
 function Upper(Input_string)
 {
 	if(typeof(Input_string)=== 'string')
 	{
 		Input_string.toUpperCase();
+		
 		return true
+		
 	}
 	else{
 		return null
