78b6bb8f0f0ccece2e29ddf80de77b2e652a5171
Varsha Tantri <varsha_tantri@mckinsey.com>
2018-01-16 10:12:13 +0530

    Upper Case Blank String

--- Hello_World.js
+++ Hello_World.js
@@ -2,7 +2,8 @@
 console.log("Return Upper Case (Failure)",Upper(12)===null)
 //test case 2
 console.log("Return Upper Case (Pass)",Upper('hi there')===true)
-
+//test case 3
+console.log("Return Upper Case (Blank string)",Upper('')===true)
 function Upper(Input_string)
 {
 	if(typeof(Input_string)=== 'string')
