7c8e8114290b10dcaa493556909a9ed3e09f998d
Varsha Tantri <varsha_tantri@mckinsey.com>
2018-01-16 10:11:13 +0530

    Upper Case Success test case

--- Hello_World.js
+++ Hello_World.js
@@ -1,5 +1,8 @@
 //test case 1
-console.log("Return Upper Case",Upper(12)===null)
+console.log("Return Upper Case (Failure)",Upper(12)===null)
+//test case 2
+console.log("Return Upper Case (Pass)",Upper('hi there')===true)
+
 function Upper(Input_string)
 {
 	if(typeof(Input_string)=== 'string')
